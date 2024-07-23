b1a5f608fa2c2f0cc380f2f568b3cdbb1a9d79fa
Jim Greenberg <jim@onsip.com>
2024-07-22 21:08:13 -0400

    cap requests to slack users.list with deferred pattern

--- src/slackService.ts
+++ src/slackService.ts
@@ -4,10 +4,19 @@ import { Cactpot } from "./cactpot";
 import { cactpotFullWidth } from "./view/cactpotFullWidth";
 import type { Avatar, LeaderboardInfo } from "./types";
 
+interface User {
+  id: string;
+  name: string;
+  image: string;
+}
+
 export class SlackService {
+  private users: User[];
+
   constructor(private app: App) {}
 
   async getUsers(channelId: string) {
+    if (this.users) return this.users;
     const { members } = await this.app.client.conversations.members({
       channel: channelId,
     });
@@ -16,7 +25,7 @@ export class SlackService {
     const { members: users } = await this.app.client.users.list();
     if (!users?.length) throw new Error();
 
-    return users
+    this.users = users
       .filter(({ is_bot }) => !is_bot)
       .filter(({ id }) => members.includes(id as string))
       .map((user) => ({
@@ -24,6 +33,8 @@ export class SlackService {
         name: user.profile?.display_name!,
         image: user.profile?.image_24!,
       }));
+
+    return this.users;
   }
 
   async beginRound(respond: RespondFn, channelId: string, games: Cactpot[]) {
