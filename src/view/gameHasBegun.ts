31da4515d4fd32a6047c9013e76789de26c63472
Jim Greenberg <jim@onsip.com>
2025-03-29 20:45:28 -0400

    fix button

--- src/view/gameHasBegun.ts
+++ src/view/gameHasBegun.ts
@@ -3,7 +3,7 @@ import * as S from "./slack";
 export function gameHasBegun(message: string, cta: string = "Play") {
   return [
     S.Section(S.Markdown(message), {
-      accessory: S.Button({ text: S.PlainText(cta), action_id: "unfinished" }),
+      accessory: S.Button({ text: cta, action_id: "unfinished" }),
     }),
   ];
 }
